931ada735bb3f1e83b2c27d6d41478fbe1ff7b86
Tim Eades <tim.eades@mac.com>
2024-11-08 08:21:34 +0000

    tidy up code

--- js/script.js
+++ js/script.js
@@ -26,7 +26,18 @@ var specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = [
+  '0', 
+  '1', 
+  '2', 
+  '3', 
+  '4', 
+  '5', 
+  '6', 
+  '7', 
+  '8', 
+  '9'
+];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
